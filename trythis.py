99cbb70a98c476c10bd3658ddee76afa58e7c9f0
William Mitchell <wfmitch@me.com>
2020-03-26 12:12:15 -0400

    updated plot

--- trythis.py
+++ trythis.py
@@ -6,7 +6,7 @@ import csv
 import matplotlib.pyplot as plt
 
 logging.basicConfig(filename='trythis.log',filemode='w',format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.DEBUG)
-
+RC = 0
 logging.info("Program Started")
 with open('.\\archived_data\\archived_time_series\\time_series_19-covid-Confirmed_archived_0325.csv', newline='') as csvfile:
      reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
@@ -16,3 +16,4 @@ plt.plot([1, 2, 3, 4])
 plt.ylabel('some numbers')
 plt.show()
 logging.info("Program Finished")
+exit(RC)
