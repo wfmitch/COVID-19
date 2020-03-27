84a71e3c061260c979016796c78fd7d5740134c9
William Mitchell <wfmitch@me.com>
2020-03-27 17:21:38 -0400

    updated time series

--- trythis.py
+++ trythis.py
@@ -3,17 +3,36 @@ import sys
 from datetime import datetime
 import logging
 import csv
+import numpy as np
 import matplotlib.pyplot as plt
-
+import math
 logging.basicConfig(filename='trythis.log',filemode='w',format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.DEBUG)
-RC = 0
 logging.info("Program Started")
-with open('.\\archived_data\\archived_time_series\\time_series_19-covid-Confirmed_archived_0325.csv', newline='') as csvfile:
-     reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    #  for row in reader:
-    #      plt.plot(reader)
-plt.plot([1, 2, 3, 4])
-plt.ylabel('some numbers')
+
+with open('csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_confirmed_global.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    dates = next(reader)
+    totals = np.zeros(len(dates)-1) 
+    log_totals = np.zeros(len(dates)-1)
+    logging.info(dates[len(dates)-1])
+    for row in reader:
+        if row[1] == 'US':
+            for days in range(4,len(dates)-1):
+                if row[days] == '': row[days]=0
+                totals[days] = totals[days] + int(row[days])
+for days in range(4,len(dates)-1):
+    log_totals[days] = math.log(totals[days]) 
+plt.figure()
+plt.suptitle('US Confirmed Cases vs Log of Confirmed Cases')
+plt.xlabel('time (days)')
+plt.ylabel('Cases')
+plt.subplot(211)
+plt.title('Confirmed Cases')
+plt.plot(dates[4:len(dates)-1],totals[4:len(dates)-1])
+plt.subplot(212)
+plt.title('Log of Confirmed Cases')
+plt.plot(dates[4:len(dates)-1],log_totals[4:len(dates)-1])
+
+
 plt.show()
 logging.info("Program Finished")
-exit(RC)
