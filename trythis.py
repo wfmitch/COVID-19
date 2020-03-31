df7edff6025f9594073cb98412a86544fbcb5215
William Mitchell <wfmitch@me.com>
2020-03-31 06:15:29 -0400

    date change

--- trythis.py
+++ trythis.py
@@ -12,15 +12,15 @@ logging.info("Program Started")
 with open('csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_confirmed_global.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
     dates = next(reader)
-    totals = np.zeros(len(dates)-1) 
-    log_totals = np.zeros(len(dates)-1)
+    totals = np.zeros(len(dates)) 
+    log_totals = np.zeros(len(dates))
     base_date = dateparser.parse(dates[4])
     true_dates = [base_date + datetime.timedelta(days=x) for x in range(len(dates)-4)]
     for row in reader:
         if row[1] == 'US':
-            for days in range(4,len(dates)-1):
+            for days in range(4,len(dates)):
                 totals[days] = totals[days] + int(row[days])
-for days in range(4,len(dates)-1):
+for days in range(4,len(dates)):
     log_totals[days] = math.log(totals[days])
 plt.figure()
 plt.suptitle('US Confirmed Cases vs Log of Confirmed Cases')
@@ -28,11 +28,9 @@ plt.xlabel('time (days)')
 plt.ylabel('Cases')
 plt.subplot(211)
 plt.title('Confirmed Cases')
-plt.plot(dates[4:len(dates)-1],totals[4:len(dates)])
+plt.plot(dates[4:len(dates)],totals[4:len(dates)])
 plt.subplot(212)
 plt.title('Log of Confirmed Cases')
-plt.plot(dates[4:len(dates)-1],log_totals[4:len(dates)])
-
-
+plt.plot(dates[4:len(dates)],log_totals[4:len(dates)])
 plt.show()
 logging.info("Program Finished")
