7093a1a004369e5d1a67cebfbacb68498ee59772
William Mitchell <wfmitch@me.com>
2020-04-08 17:14:46 -0400

    updated import

--- trythis.py
+++ trythis.py
@@ -1,36 +1,37 @@
-import os
-import sys
-import datetime
-import dateparser
-import logging
+import os,sys,dotenv,logging,dateparser,random,argparse
+from datetime import datetime
+
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-logging.basicConfig(filename='trythis.log',filemode='w',format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.DEBUG)
-logging.info("Program Started")
-with open('csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_confirmed_global.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile)
-    dates = next(reader)
-    totals = np.zeros(len(dates)-4) 
-    log_totals = np.zeros(len(dates)-4)
-    base_date = dateparser.parse(dates[4])
-    true_dates = [base_date + datetime.timedelta(days=x) for x in range(len(dates)-4)]
-    for row in reader:
-        if row[1] == 'US':
-            for days in range(4,len(dates)):
-                totals[days-4] = totals[days-4] + int(row[days])
-for days in range(4,len(dates)):
-    log_totals[days-4] = math.log(totals[days-4])
-plt.figure()
-plt.suptitle('US Confirmed Cases vs Log of Confirmed Cases')
-plt.xlabel('time (days)')
-plt.ylabel('Cases')
-plt.subplot(211)
-plt.title('Confirmed Cases')
-plt.plot(true_dates,totals)
-plt.subplot(212)
-plt.title('Log of Confirmed Cases')
-plt.plot(true_dates,log_totals)
-plt.show()
-logging.info("Program Finished")
+from _datetime import timedelta
+
+if __name__ == '__main__':
+    logging.basicConfig(filename='trythis.log',filemode='w',format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.DEBUG)
+    logging.info("Program Started")
+    with open('csse_covid_19_data\\csse_covid_19_time_series\\time_series_covid19_confirmed_global.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        dates = next(reader)
+        totals = np.zeros(len(dates)-4) 
+        log_totals = np.zeros(len(dates)-4)
+        base_date = dateparser.parse(dates[4])
+        true_dates = [base_date + timedelta(days=x) for x in range(len(dates)-4)]
+        for row in reader:
+            if row[1] == 'US':
+                for days in range(4,len(dates)):
+                    totals[days-4] = totals[days-4] + int(row[days])
+    for days in range(4,len(dates)):
+        log_totals[days-4] = math.log(totals[days-4])
+    plt.figure()
+    plt.suptitle('US Confirmed Cases vs Log of Confirmed Cases')
+    plt.xlabel('time (days)')
+    plt.ylabel('Cases')
+    plt.subplot(211)
+    plt.title('Confirmed Cases')
+    plt.plot(true_dates,totals)
+    plt.subplot(212)
+    plt.title('Log of Confirmed Cases')
+    plt.plot(true_dates,log_totals)
+    plt.show()
+    logging.info("Program Finished")
